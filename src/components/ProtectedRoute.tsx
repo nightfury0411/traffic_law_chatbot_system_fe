7b55a8f844df11f7a5c7117edf1a4fd84f86fe21
Tuan An <ancntse171335@fpt.edu.vn>
2025-07-13 01:34:58 +0700

    Fix login

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import React from "react";
+import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   // Thay thế logic này bằng cách kiểm tra xác thực của bạn
-  const isAuthenticated = localStorage.getItem('authToken'); // Ví dụ: kiểm tra token
+  const isAuthenticated = localStorage.getItem("token"); // Ví dụ: kiểm tra token
 
   if (!isAuthenticated) {
     // Nếu chưa đăng nhập, chuyển hướng đến trang sign-in
